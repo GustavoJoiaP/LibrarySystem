0e7b4a3a897ef47c15c25a9d73bffcffe923f4d7
Gustavo Joia Paziani <joia.gustavo@gmail.com>
2023-04-30 11:26:33 -0300

    Created the CRUD of books

--- src/app.js
+++ src/app.js
@@ -4,9 +4,9 @@ const app = express();
 
 app.use(express.json())
 
-const books = [
-    {id: 1, "Titulo": "Domain Drivem Design"},
-    {id: 2, "Titulo": "Clean Code"}
+let books = [
+    {Id: 1, "Titulo": "Domain Drivem Design"},
+    {Id: 2, "Titulo": "Clean Code"}
 ]
 
 app.get('/', (req, res) => {
@@ -17,9 +17,32 @@ app.get('/books', (req, res) => {
     res.status(200).json(books)
 })
 
+app.get('/books/:Id', (req, res) => {
+    let index = FindBook(req.params.Id);
+    res.json(books[index]);
+})
+
 app.post('/books', (req, res) =>{
     books.push(req.body);
     res.status(201).send('Book was register!')
 })
+
+app.put('/books/:Id', (req, res) => {
+    let index = FindBook(req.params.Id);
+    books[index].Titulo = req.body.Titulo;
+    res.json(books);
+})
+
+app.delete('/books/:Id', (req, res) => {
+    let {id} = req.params;
+    let index = FindBook(id);
+    books.splice(index, 1);
+    res.send(`Book ${id} was removed with success`);
+})
+
+
+function FindBook(id){
+    return books.findIndex(book => book.Id == id)
+}
 
 export default app
