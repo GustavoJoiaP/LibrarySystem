f63b9c01f611bea23a80b2ad3ce969d588ddb900
Gustavo Joia Paziani <joia.gustavo@gmail.com>
2023-04-30 10:55:47 -0300

    created rout to send data to register one book

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json())
+
 const books = [
     {id: 1, "Titulo": "Domain Drivem Design"},
     {id: 2, "Titulo": "Clean Code"}
@@ -13,6 +15,11 @@ app.get('/', (req, res) => {
 
 app.get('/books', (req, res) => {
     res.status(200).json(books)
+})
+
+app.post('/books', (req, res) =>{
+    books.push(req.body);
+    res.status(201).send('Book was register!')
 })
 
 export default app
